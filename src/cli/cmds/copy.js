2c1ff12b1fce7bd0ab6bb0536e0f1226b181b4fc
Gitlab-CI <dev@kopaxgroup.com>
2018-11-14 23:35:56 +0700

    fix(cmds): fix copy and intl

--- src/cli/cmds/copy.js
+++ src/cli/cmds/copy.js
@@ -2,6 +2,27 @@
 const async = require('async');
 const path = require('path');
 const fs = require('fs');
+
+const { spawn: spawnDefault, exec: execDefault } = require('child_process');
+
+const exec = (command, cb) => {
+  execDefault(command, {
+    maxBuffer: 1024 * 1024,
+  }, cb);
+};
+const spawn = (command, cb) => {
+  const split = command.split(' ');
+  const program = split[0];
+  const args = split.slice(1);
+  const child = spawnDefault(program, args || []);
+  const outputList = [];
+  child.stdout.setEncoding('utf8');
+  child.stderr.setEncoding('utf8');
+  child.stdout.on('data', (data) => outputList.push(data) && console.log(data.replace(/\n$/, '')));
+  child.stderr.on('data', (data) => outputList.push(data) && console.log(data.replace(/\n$/, '')));
+  child.on('close', (code) => code === 1 ? cb(new Error(`child process exited with code ${code}`, [outputList.join('')])) : cb(null, [outputList.join('')]));
+};
+
 exports.command = 'copy';
 exports.desc = 'Copy template documentation from `internal/templates/docs` to `docs`.';
 exports.builder = (yargs) => yargs
