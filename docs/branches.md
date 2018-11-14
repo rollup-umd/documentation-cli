a3d30a504e7ff002005b0a4c2f575d6f9f4802b4
Gitlab-CI <dev@kopaxgroup.com>
2018-11-14 19:20:46 +0700

    chore(init): Initialization

---
+++ docs/branches.md
@@ -0,0 +1,4 @@
+1. `master` is used to release the version. 
+1. `master` only accept merge requests from `dev`
+1. `dev` is the development branch. It should be used by developers for applying their merge requests.
+
---
+++ styleguide/prepare.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+npx rollup-umd-scripts doc variable \
+  PACKAGE_NAME=$(node -p "require('./package.json').name") \
+  PACKAGE_DESCRIPTION="$(node -p "require('./package.json').description")" \
+  PACKAGE_VERSION=$(node -p "require('./package.json').version") \
+  PACKAGE_PEERS="$(npx rollup-umd-scripts peer npm-install-cmd)" \
+  NODE_VERSION=$(node --version) \
+  NPM_VERSION=$(npm --version) \
+  CI_REPOSITORY_URL=${CI_REPOSITORY_URL} \
+  CI_PROJECT_URL=${CI_PROJECT_URL} \
+  CI_PROJECT_NAMESPACE=${CI_PROJECT_NAMESPACE} \
+  CI_PROJECT_NAME=${CI_PROJECT_NAME} \
+  IMG_SHIELD_PUBLISHING=$(npx rollup-umd-scripts publish status --badge)
+
+npx rollup-umd-scripts doc add-section -n 'Code of conduct' -a 'FAQ' -c 'CODE_OF_CONDUCT.md' --force
+npx rollup-umd-scripts doc add-section -n 'Changelog' -a 'Code of conduct' -c 'CHANGELOG.md' --force
